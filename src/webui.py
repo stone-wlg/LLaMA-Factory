edd2a477d255948de2507da80e66622e4d264f16
stone-wlg <stone_wlg@163.com>
2024-06-24 15:59:20 +0800

    ...
    Signed-off-by: stone-wlg <stone_wlg@163.com>

--- src/webui.py
+++ src/webui.py
@@ -20,7 +20,9 @@ from llamafactory.webui.interface import create_ui
 def main():
     gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.environ.get("GRADIO_SERVER_NAME", "0.0.0.0")
-    create_ui().queue().launch(share=gradio_share, server_name=server_name, inbrowser=True)
+    username = os.environ.get("GRADIO_USERNAME", "admin")
+    password = os.environ.get("GRADIO_PASSWORD", "admin")    
+    create_ui().queue().launch(auth=(username, password), share=gradio_share, server_name=server_name, inbrowser=True)
 
 
 if __name__ == "__main__":
